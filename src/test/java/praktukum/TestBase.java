632bd51ba38c15c68ee00968507e0990ced61700
f4natic <infr4rot@gmail.com>
2023-11-10 01:40:07 +0700

    Тесты на создание пользователя

---
+++ src/test/java/praktukum/TestBase.java
@@ -0,0 +1,60 @@
+package praktukum;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.qameta.allure.Step;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.BeforeClass;
+import praktukum.model.user.User;
+
+public class TestBase {
+
+    private static final String BASE_URL = System.getenv("url") == null ? "https://stellarburgers.nomoreparties.site/" : System.getenv("url");
+    private static final String API = System.getenv("api") == null ? "/api" : System.getenv("api");
+    private static final Parser PARSER = Parser.fromContentType("application/json;charset=utf-8");
+    protected static RequestSpecification specification;
+    protected static ObjectMapper mapper;
+
+    @BeforeClass
+    @Step("Создание Request Specification и ObjectMapper'а")
+    public static void setUp() {
+        RequestSpecBuilder builder = new RequestSpecBuilder()
+                .setBaseUri(BASE_URL)
+                .setBasePath(API)
+                .setContentType(PARSER.getContentType())
+                .setAccept(ContentType.JSON);
+        specification = builder.build();
+        mapper = new ObjectMapper();
+    }
+
+    @Step("Создание пользователя")
+    public Response createUser(User user) throws JsonProcessingException {
+        return RestAssured.given(specification)
+                .body(mapper.writeValueAsString(user))
+                .post("/auth/register");
+    }
+
+    @Step("Авторизация пользователя")
+    public Response login(User user) throws JsonProcessingException {
+        return RestAssured.given(specification)
+                .body(mapper.writeValueAsString(user))
+                .post("/auth/login");
+    }
+
+    @Step("Выход пользователя из системы")
+    public Response logout() {
+        return null;
+    }
+
+    @Step("Удаление пользователя")
+    public Response deleteUser(String token) {
+        return RestAssured.given(specification)
+                .header("Authorization", String.format("%s", token))
+                .delete("/auth/user");
+    }
+}
--- src/test/java/praktukum/UserCreationTest.java
+++ src/test/java/praktukum/UserCreationTest.java
@@ -1,4 +1,39 @@
 package praktukum;
 
-public class UserCreationTest {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.qameta.allure.Epic;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Assert;
+import org.junit.Test;
+import praktukum.model.user.User;
+import praktukum.model.user.ReturnedData;
+
+@Epic("Тесты на создание пользователя")
+public class UserCreationTest extends TestBase {
+    @Test
+    @DisplayName("Создание уникального пользователя")
+    public void shouldSuccessCreateUniqUserTest() throws JsonProcessingException {
+        User user = new User("f4natic@example.org", "f4natic", "f4natic");
+        ReturnedData createUser = createUser(user).as(ReturnedData.class);
+        deleteUser(login(user).as(ReturnedData.class).getAccessToken()).as(ReturnedData.class);
+        Assert.assertTrue("Не удалось создать пользователя", createUser.getSuccess());
+    }
+
+    @Test
+    @DisplayName("Создание пользователя, который уже зарегистрировался")
+    public void shouldNoSuccessCreateUser() throws JsonProcessingException {
+        User user = new User("f4natic@example.org", "f4natic", "f4natic");
+        createUser(user);
+        ReturnedData createUser = createUser(user).as(ReturnedData.class);
+        deleteUser(login(user).as(ReturnedData.class).getAccessToken()).as(ReturnedData.class);
+        Assert.assertFalse("Что-то пошло не так и пользователь создался", createUser.getSuccess());
+    }
+
+    @Test
+    @DisplayName("Создание пользователя, без обязательных полей")
+    public void shouldNoSuccessCreateUserIfRequiredFieldIsNull() throws JsonProcessingException {
+        User user = new User();
+        ReturnedData createUser = createUser(user).as(ReturnedData.class);
+        Assert.assertFalse("Что-то пошло не так и пользователь создался", createUser.getSuccess());
+    }
 }
